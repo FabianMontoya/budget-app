635d8787b9bb4daf04a5770e37b0847afd48ea14
Fabian Dario Montoya <37662924+FabianMontoya@users.noreply.github.com>
2023-04-27 22:27:09 -0500

    add dayjs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import '@/router/guards';
+import { onBeforeUnmount, onMounted, reactive } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
+
+const app = reactive({
+  isOnline: true
+});
+
+const setIsOnline = () => (app.isOnline = true);
+const setIsOffLine = () => (app.isOnline = false);
+
+onMounted(() => {
+  window.addEventListener('online', setIsOnline);
+  window.addEventListener('offline', setIsOffLine);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('online', setIsOnline);
+  window.removeEventListener('offline', setIsOffLine);
+});
 </script>
 
 <template>
