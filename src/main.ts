ced35a1472fac157c37d5d607651c2760aafabc2
Fabian Dario Montoya <37662924+FabianMontoya@users.noreply.github.com>
2023-04-27 22:38:16 -0500

    add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
-import App from './App.vue';
-import router from './router';
-
+import '@/fontawesome';
+import '@/nprogress';
 import './assets/main.css';
 
-import '@/nprogress';
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
