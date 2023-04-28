916d949001a63b994a7a84859f22b99d32442d41
Fabian Dario Montoya <37662924+FabianMontoya@users.noreply.github.com>
2023-04-27 22:23:12 -0500

    add nprogress for routes navigation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+import '@/nprogress';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/views/errors/404View.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <article>
+    <h1>URL or source not found - 404</h1>
+  </article>
+</template>
