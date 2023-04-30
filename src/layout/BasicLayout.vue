3ddbfe4cce520c13cac0d51cb10e113f5107556c
Roberto Ochoa <jroberto.ochoaa@gmail.com>
2023-04-30 17:23:58 -0600

    [BA-0001] | Improve login page

--- src/layout/BasicLayout.vue
+++ src/layout/BasicLayout.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <main class="min-h-screen bg-gray-100">
+  <main class="min-h-screen bg-gray-100 bg-opacity-10">
     <RouterView />
   </main>
 </template>
