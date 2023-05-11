652e80346d85d0211a17de2042e3845ff681c04b
Fabian Dario Montoya <37662924+FabianMontoya@users.noreply.github.com>
2023-05-11 16:11:35 -0500

    add vuei18n and vue3-mq libraries

---
+++ src/i18n/translations/index.ts
@@ -0,0 +1,15 @@
+import global_EN from './global/en.json';
+import global_ES from './global/es.json';
+import login_EN from './login/en.json';
+import login_ES from './login/es.json';
+
+export default {
+  es: {
+    global: global_ES,
+    login: login_ES
+  },
+  en: {
+    global: global_EN,
+    login: login_EN
+  }
+};
